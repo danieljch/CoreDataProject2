cdc44a54d98b6c6260d8a04692b65b2e79ae1a1a
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-04 20:39:08 -0400

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
