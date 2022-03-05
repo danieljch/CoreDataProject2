324d7ef19fbe3425f95ff7e42b669f93cd5bd426
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-04 20:45:07 -0400

    CoreData created

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 2/3/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
